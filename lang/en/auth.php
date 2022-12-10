5535431c9d16d70413656f4adaa984559d5cea9c
Dragan <s.dragan.95@outlook.it>
2022-12-10 12:00:16 +0100

    change auth error messages

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => "L'account non esiste.",
+    'password' => 'La password inserita non è corretta.',
+    'throttle' => 'Troppi tentativi. Riprova fra :seconds secondi.',
 
 ];
