25448b743fd8a23c1aa12262b672cd04c8cf9ffc
Dragan <s.dragan.95@outlook.it>
2022-12-10 11:54:29 +0100

    chaneg login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,53 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.welcome')
 
+@section('content')
+
+    <div class="form-container py-12">
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <form method="POST" action="{{ route('login') }}">
+        <div  class="flex justify-center">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-green-500" />
+            </a>
+        </div>
+
+        <form method="POST" action="{{ route('login') }}" class="form">
             @csrf
 
-            <!-- Email Address -->
+            {{-- EMAIL --}}
             <div>
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <x-form.label :text="__('Email')" :for="__('email')" />
+                <x-form.input :type="__('email')" :name="__('email')" :oldValue="__('email')"
+                    required autofocus />
+                <x-form.input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
-            <!-- Password -->
+            {{-- PASSWORD --}}
             <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <x-form.label :text="__('Password')" :for="__('password')" />
+                <x-form.input :type="__('password')" :name="__('password')" :oldValue="__('password')"
+                    required autocomplete="current-password" />
+                <x-form.input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
 
             <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-green-600 shadow-sm focus:ring-0" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Ricordami') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Password dimenticata?') }}
                     </a>
                 @endif
 
-                <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-primary-button>
+                <x-buttons.primary-button :text="__('Log in')" :class="__('ml-4')" />
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+@endsection
