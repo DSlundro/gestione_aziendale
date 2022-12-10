cce4bf3e89d06133444e03609e59c5e6b75e1f2e
Dragan <s.dragan.95@outlook.it>
2022-12-10 13:06:00 +0100

    remove reset password link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,11 +40,11 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+                {{-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Password dimenticata?') }}
                     </a>
-                @endif
+                @endif --}}
 
                 <x-buttons.primary-button :text="__('Log in')" :class="__('ml-4')" />
             </div>
